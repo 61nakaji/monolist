5e2f7cedd39451f24a68e95dccaca4f18a75e0a5
EC2 Default User <testtest@gmail.com>
2018-09-02 06:47:04 +0000

    user register

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//top page
 Route::get('/', 'WelcomeController@index');
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
